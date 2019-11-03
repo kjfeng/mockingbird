f46639b14323fb78b08fb4cf46b2fb25bf63ba92
Kevin Feng <kjfeng@princeton.edu>
2019-11-02 23:54:18 -0400

    added sign up panel

---
+++ landing-script.js
@@ -0,0 +1,6 @@
+function switchToSignup() {
+  document.getElementsByClassName("login-title-wrapper")[0].style.display = "none";
+  document.getElementsByClassName("login-form-wrapper")[0].style.display = "none";
+  document.getElementsByClassName("signup-title-wrapper")[0].style.display = "block";
+  document.getElementsByClassName("signup-form-wrapper")[0].style.display = "block";
+}
