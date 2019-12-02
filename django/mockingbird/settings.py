897d1291d9e08a5cfa6e5874c65dcb9ffacd6357
joudrew <joudrewjoudrew11@gmail.com>
2019-12-02 11:57:20 -0500

    modify db settings

--- django/mockingbird/settings.py
+++ django/mockingbird/settings.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -91,9 +90,6 @@ DATABASES = {
         'PORT': '54320',
     }
 }
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
