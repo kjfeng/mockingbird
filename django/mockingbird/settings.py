ba1561422043e18a4ebb3b767077580164a7a0ec
joudrew <joudrewjoudrew11@gmail.com>
2019-12-02 00:51:42 -0500

    allowed hosts

--- django/mockingbird/settings.py
+++ django/mockingbird/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'j!8l9$b(2=ngj&=4%+ds4$si1p8nq&%760+w3i43w@8148)040'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [teammockingbird333.herokuapp.com]
 
 
 # Application definition
