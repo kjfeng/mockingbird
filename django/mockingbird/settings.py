45d537eb155dcbc20ca2ecdc76833d6f489d4a0c
Annie Zhou <pianistanniez@gmail.com>
2019-12-07 17:36:19 -0500

    changed redis_url

--- django/mockingbird/settings.py
+++ django/mockingbird/settings.py
@@ -22,6 +22,7 @@ SECRET_KEY = 'j!8l9$b(2=ngj&=4%+ds4$si1p8nq&%760+w3i43w@8148)040'
 DEBUG =True #False
 
 ALLOWED_HOSTS = ['teammockingbird333.herokuapp.com', '127.0.0.1']
+REDIS_URL = 'redis://h:ped0612c2ab8f1af2281848fbdce999d9e4c5f420ebf81c6c028a1cc85602b55a@ec2-54-164-134-74.compute-1.amazonaws.com:22949'
 BROKER_URL = 'redis://localhost:6379'
 #BROKER_URL = os.environ.get("REDISCLOUD_URL", "django://")
 
