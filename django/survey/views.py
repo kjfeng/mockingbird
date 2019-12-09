d05673f60a891c53deb27b17d73f5743dcc48c24
Annie Zhou <pianistanniez@gmail.com>
2019-12-09 06:32:42 -0500

    added drew's phone numbers

--- django/survey/views.py
+++ django/survey/views.py
@@ -90,45 +90,45 @@ def survey(request):
 @login_required(login_url='/login/')
 def survey_complete(request):
     return render(request, 'survey/survey_complete.html')
-        if form.is_valid():
-            target = User.objects.filter(username=request.user.profile.match_name)[0]
-            #print(target, file=stderr)
-            #print(target.profile.industry, file=stderr)
+    if form.is_valid():
+        target = User.objects.filter(username=request.user.profile.match_name)[0]
+        #print(target, file=stderr)
+        #print(target.profile.industry, file=stderr)
 
-            # update information about user
-            request.user.profile.match_name = ""
-            request.user.profile.is_matched = False
-            request.user.save()
+        # update information about user
+        request.user.profile.match_name = ""
+        request.user.profile.is_matched = False
+        request.user.save()
 
-            # update information about user's match
-            if form.cleaned_data['did_meet'] == 'no':
-                #print("did not meet", file=stderr)
-                target.statistics.no_show += 1
-                target.statistics.save()
-            else:
-                #print("did meet", file=stderr)
+        # update information about user's match
+        if form.cleaned_data['did_meet'] == 'no':
+            #print("did not meet", file=stderr)
+            target.statistics.no_show += 1
+            target.statistics.save()
+        else:
+            #print("did meet", file=stderr)
 
-                # update the target's statistics
-                target.statistics.tot_interview += 1
-                current_total_rate = Decimal(target.statistics.rate)*target.statistics.tot_interview
-                target.statistics.rate = (current_total_rate + Decimal(form.cleaned_data['friendly'])/Decimal(5.0))/target.statistics.tot_interview
+            # update the target's statistics
+            target.statistics.tot_interview += 1
+            current_total_rate = Decimal(target.statistics.rate)*target.statistics.tot_interview
+            target.statistics.rate = (current_total_rate + Decimal(form.cleaned_data['friendly'])/Decimal(5.0))/target.statistics.tot_interview
 
-                current_total_late = Decimal(target.statistics.late)*target.statistics.tot_interview
-                target.statistics.late += (current_total_late + Decimal(form.cleaned_data['friendly'])/Decimal(5.0))/target.statistics.tot_interview
-                target.statistics.save()
+            current_total_late = Decimal(target.statistics.late)*target.statistics.tot_interview
+            target.statistics.late += (current_total_late + Decimal(form.cleaned_data['friendly'])/Decimal(5.0))/target.statistics.tot_interview
+            target.statistics.save()
 
-                # send email if there is feedback
-                if form.cleaned_data['comment'] != "":
-                    subject = 'Comment Left'
-                    message = form.cleaned_data['comment'] + "\n from <b>" + str(request.user.username) + "</b>"
-                    send_mail(
-                        subject,
-                        message,
-                        'teammockingbird333@gmail.com',
-                        ['teammockingbird333@gmail.com'],
-                        fail_silently=True,
-                    )
-            return redirect('survey:survey_complete')
+            # send email if there is feedback
+            if form.cleaned_data['comment'] != "":
+                subject = 'Comment Left'
+                message = form.cleaned_data['comment'] + "\n from <b>" + str(request.user.username) + "</b>"
+                send_mail(
+                    subject,
+                    message,
+                    'teammockingbird333@gmail.com',
+                    ['teammockingbird333@gmail.com'],
+                    fail_silently=True,
+                )
+        return redirect('survey:survey_complete')
     else:
         form = SurveyForm()
         form.fields['did_meet'].label = "Did you meet with your match?"
