5cbe7c8e0dc85b9a65b553ff65215c4cd2504a97
joudrew <joudrewjoudrew11@gmail.com>
2019-12-09 01:12:36 -0500

    change broker

--- django/mockingbird/celery.py
+++ django/mockingbird/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mockingbird.settings')
 redis = 'redis://localhost:6379'
-app = Celery('mockingbird', broker=redis)
+app = Celery('mockingbird')#, broker=redis)
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
