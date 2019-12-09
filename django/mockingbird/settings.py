b01a85e7286db4c3f9796c4836f9ddab9faa505a
Annie Zhou <pianistanniez@gmail.com>
2019-12-09 02:19:00 -0500

    changed procfile, settings, .env

--- django/mockingbird/settings.py
+++ django/mockingbird/settings.py
@@ -22,10 +22,12 @@ SECRET_KEY = 'j!8l9$b(2=ngj&=4%+ds4$si1p8nq&%760+w3i43w@8148)040'
 DEBUG =True #False
 
 ALLOWED_HOSTS = ['teammockingbird333.herokuapp.com', '127.0.0.1', 'localhost']
-#REDIS_URL = 'redis://h:ped0612c2ab8f1af2281848fbdce999d9e4c5f420ebf81c6c028a1cc85602b55a@ec2-54-164-134-74.compute-1.amazonaws.com:22949'
+REDIS_URL = 'redis://h:ped0612c2ab8f1af2281848fbdce999d9e4c5f420ebf81c6c028a1cc85602b55a@ec2-54-164-134-74.compute-1.amazonaws.com:22949'
 #BROKER_URL = 'redis://localhost:6379'
 #BROKER_URL = os.environ.get("REDISCLOUD_URL", "django://")
-BROKER_URL = 'amqp://agixgbiz:rz5IW-...@shark.rmq.cloudamqp.com/agixgbiz'
+#BROKER_URL = 'amqp://agixgbiz:rz5IW-...@shark.rmq.cloudamqp.com/agixgbiz'
+BROKER_URL= os.environ['REDIS_URL']
+CELERY_RESULT_BACKEND=os.environ['REDIS_URL']
 BROKER_POOL_LIMIT = 3
 
 
@@ -36,7 +38,7 @@ BROKER_POOL_LIMIT = 3
 
 #BROKER_POOL_LIMIT = None
 
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+#CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['pickle']
 CELERY_TASK_SERIALIZER = 'pickle'
 
