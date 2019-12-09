8dc3ce9cdf5f89707bb65eea90b1d1a4047b59d4
Damon Caron <dcaron@princeton.edu>
2019-12-09 03:55:02 -0500

    match caching + excluding recent matches

--- django/match/admin.py
+++ django/match/admin.py
@@ -1,5 +1,7 @@
-# from django.contrib import admin
-# from onboard.models import Profile
-#
+from django.contrib import admin
+from onboard.models import Profile
+from .models import Cached_Matches, Recent_Matches
+
 # # Register your models here.
-# admin.site.register(Profile)
+admin.site.register(Cached_Matches)
+admin.site.register(Recent_Matches)
