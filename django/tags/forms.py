102d863d132c139b4fd43d31137ffdede7327972
Annie Zhou <pianistanniez@gmail.com>
2019-12-30 21:12:18 -0500

    fixed form field ordering + added help text

--- django/tags/forms.py
+++ django/tags/forms.py
@@ -7,37 +7,37 @@ from onboard.models import Profile, INDUSTRY_CHOICES, INDUSTRY_MATCH_CHOICE
 
 
 class TagForm(UserChangeForm):
-  industry_choice_1 = forms.ChoiceField(choices=INDUSTRY_CHOICES)
-  industry_choice_2 = forms.ChoiceField(choices=INDUSTRY_CHOICES)
-  industry_match = forms.ChoiceField(choices=INDUSTRY_MATCH_CHOICE)
+  industry_choice_1 = forms.ChoiceField(choices=INDUSTRY_CHOICES, help_text="Choose your preferred/primary industry.")
+  industry_choice_2 = forms.ChoiceField(choices=INDUSTRY_CHOICES, help_text="If applicable, choose your secondary industry.")
+  industry_match = forms.ChoiceField(choices=INDUSTRY_MATCH_CHOICE, help_text="Choose which of your chosen industries you'd like to be matched in. Or find matches in both (or neither)!")
   password = None
   class Meta:
     model = Profile
-    fields = {
+    fields = (
       'industry_choice_1',
       'industry_choice_2',
       'industry_match',
-    }
+    )
 
   def is_valid(self):
-     
+
         # run the parent validation first
         valid = super(TagForm, self).is_valid()
- 
+
         # we're done now if not valid
         if not valid:
             return valid
 
+        if (self.cleaned_data['industry_choice_1'] == 'None' or self.cleaned_data['industry_choice_1'] == None):
+            return 'No Industry 1'
+            
         if (self.cleaned_data['industry_choice_1'] == self.cleaned_data['industry_choice_2']):
             return 'Dup Industry'
 
-        if (self.cleaned_data['industry_choice_1'] == 'None' or self.cleaned_data['industry_choice_1'] == None):
-            return 'No Industry 1'
- 
         if (self.cleaned_data['industry_choice_2'] == 'None' and (self.cleaned_data['industry_match'] == 'Industry 2' or self.cleaned_data['industry_match'] == 'Both')):
             return 'Industry Match'
 
-        
-       
+
+
         # all good
         return 1
