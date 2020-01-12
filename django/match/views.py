ef7f77fcdce2dc5058c32e785f381964a9cddca3
Annie Zhou <pianistanniez@gmail.com>
2020-01-12 16:56:49 -0500

    send survey issues

--- django/match/views.py
+++ django/match/views.py
@@ -493,9 +493,9 @@ def accept_request(request):
             })
             target.email_user(subject, message)
 
-        send_survey(request, target, str(target.profile.match_name), str(t_username))
-        # send_time = timezone.now() + timedelta(seconds=30)
-        # send_survey.apply_async(eta=send_time, args=(request.user, target, current_site))
+        #send_survey(request, target, str(target.profile.match_name), str(t_username))
+        send_time = timezone.now() + timedelta(seconds=30)
+        send_survey.apply_async(eta=send_time, args=(request.user, target, current_site))
         matchedUser = MatchedUser(username = str(target.username), email = str(target.email),
                     industry1 = str(target.profile.industry_choice_1), industry2 = str(target.profile.industry_choice_2))
 
