f6c0289d48be392fd8e083061f410d8e6ebd9d4f
Annie Zhou <pianistanniez@gmail.com>
2020-01-13 17:56:04 -0500

    changed organization of modules bc procfile cannot access

--- routing.py
+++
@@ -1,9 +0,0 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-import chat.routing
-
-application = ProtocolTypeRouter({
-    # (http->django views is added by default)
-    'websocket': URLRouter(
-        chat.routing.websocket_urlpatterns
-        ),
-})
